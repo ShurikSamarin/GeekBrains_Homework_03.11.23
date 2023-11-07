e8076001e2fe22533d5387d7e2ca5cd604601a44
Alexander Burtsev <sttp2002@gmail.com>
2023-11-07 09:22:30 +0300

    ToyQueue updated

--- ToyQueue.java
+++ ToyQueue.java
@@ -1,61 +1,72 @@
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Random;
 
+class Toy {
+    private int id;
+    private String name;
+    private int weight;
+
+    public Toy(int id, String name, int weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
+
 public class ToyQueue {
-    private int[] toyWeights;
+    public static void main(String[] args) {
+        Toy toy1 = new Toy(1, "конструктор", 2);
+        Toy toy2 = new Toy(2, "робот", 2);
+        Toy toy3 = new Toy(3, "кукла", 6);
 
-    private final Queue<String> toyQueue = new PriorityQueue<>(10, (s1, s2) -> {
-        int weight1 = toyWeights[Integer.parseInt(s1) - 1];
-        int weight2 = toyWeights[Integer.parseInt(s2) - 1];
-        return Integer.compare(weight2, weight1);
-    });
+        PriorityQueue<Toy> queue = new PriorityQueue<>((t1, t2) -> t2.getWeight() - t1.getWeight());
+        
+        queue.offer(toy1);
+        queue.offer(toy2);
+        queue.offer(toy3);
+       
+        
 
-    public ToyQueue(String toyId, String toyName, String toyWeight) {
-        toyId.split(" ");
-        toyName.split(" ");
-        String[] weightArr = toyWeight.split(" ");
-        toyWeights = new int[weightArr.length];
-        for (int i = 0; i < weightArr.length; i++) {
-            toyWeights[i] = Integer.parseInt(weightArr[i]);
-        }
-    }
-
-    public void put(String toy) {
-        toyQueue.offer(toy);
-    }
-
-    public String get() {
         Random random = new Random();
-        int num = random.nextInt(10) + 1;
-        if (num <= 2) {
-            return toyQueue.poll();
-        } else if (num <= 4) {
-            return "2";
-        } else {
-            return "3";
-        }
-    }
-
-    public void writeToFile(String filename) {
-        try (FileWriter writer = new FileWriter(filename)) {
+        try {
+            FileWriter writer = new FileWriter("output.txt");
             for (int i = 0; i < 10; i++) {
-                writer.write(get() + "\n");
+                int randomNumber = random.nextInt(100);
+                Toy toy = get(queue, randomNumber);
+                writer.write(toy.getId() + "\n");
             }
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    
 
-    public static void main(String[] args) {
-        ToyQueue toyQueue = new ToyQueue("1 2 3", "конструктор робот кукла", "2 2 6");
-        toyQueue.put("1");
-        toyQueue.put("2");
-        toyQueue.put("3");
-        toyQueue.writeToFile("output.txt");
+    public static Toy get(PriorityQueue<Toy> queue, int randomNumber) {
+        int totalWeight = queue.stream().mapToInt(Toy::getWeight).sum();
+        int cumulativeWeight = 0;
+        for (Toy toy : queue) {
+            cumulativeWeight += toy.getWeight();
+            if (randomNumber <= (cumulativeWeight * 100 / totalWeight)) {
+                return toy;
+            }
+        }
+        return null;
     }
 }
 
